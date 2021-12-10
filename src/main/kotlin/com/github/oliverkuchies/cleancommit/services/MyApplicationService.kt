952b1a1a19fa97265d61fa93f1da2fbad6e51466
GitHub Action <action@github.com>
2021-12-10 00:44:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/oliverkuchies/cleancommit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/oliverkuchies/cleancommit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.oliverkuchies.cleancommit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.oliverkuchies.cleancommit.MyBundle
 
 class MyApplicationService {
 
